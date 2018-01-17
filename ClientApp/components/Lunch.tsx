9ff3b9737f560950203a1f3adba2d345f741435a
Olexandr Nikolaevsky <olexandr.nikolaevsky@itera.no>
2018-01-17 15:25:02 +0200

    Add Lunch component

---
+++ ClientApp/components/Lunch.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+
+export class Lunch extends React.Component {
+    render() {
+        return (
+            <div>
+                {/* <h2>Lunch {this.props.no}</h2> */}
+                <hr/>
+                1. Красный борщ
+                2. Отбивная (свинина)
+                3. Булгур с овощами
+                4. Оливье
+            </div>
+        );
+    }
+}
--- ClientApp/components/LunchMenu.tsx
+++ ClientApp/components/LunchMenu.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import { RouteComponentProps } from 'react-router';
+import { Lunch } from './Lunch';
 
 export class LunchMenu extends React.Component<RouteComponentProps<{}>, {}> {
     public render() {
-        return <div> 
-            <h1>Lunch Menu for 30.12.2017</h1>
-        </div>;
+        return ( 
+            <div> 
+                <h1>Lunch Menu for 17.01.2018</h1>
+                <hr/>
+                <Lunch />
+            </div>
+        );
     }
 }
